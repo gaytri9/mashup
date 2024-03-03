9c8795030c37acbcae884deccc2c3d7ca60c5c2c
gaytri9 <139551519+gaytri9@users.noreply.github.com>
2024-03-03 18:13:55 +0530

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,6 @@ Original file is located at
 
 import streamlit as st
 import os
-from moviepy.editor import *
 from pytube import YouTube
 from youtubesearchpython import VideosSearch
 from email.message import EmailMessage
